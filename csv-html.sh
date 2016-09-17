b7ee50d851955ac0d222a74757862109e6404ed7
Marijus Siliunas <marijus.siliunas@gmail.com>
2016-09-17 15:35:30 +0300

    Add help and validity checks

--- csv-html.sh
+++ csv-html.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#! /usr/bin/env bash
 # Marijus Siliunas PS 2 gr. 4 k. 1310490
 # Steps:
 # 1. Validate input file
@@ -7,8 +7,50 @@
 # 4. Create file with input filename
 # 5. Exit
 
-IFS=,
-input_file=$1
+function help {
+    echo -e "Convert CSV file to valid HTML table.\n"
+    echo "Required arguments:"
+    echo "	-i	Path to input file"
+    echo "Optional arguments:"
+    echo "	-o	Output file path (default: $input_filename.html)"
+    echo "	-d	Delimiter (default: ,)"
+}
+
+if [ $# -eq 0 ]; then
+    help
+    exit
+fi
+
+while getopts ":o:i:d" opt; do
+    case $opt in
+    i) input_file="$OPTARG"
+    ;;
+    d) delimiter="$OPTARG"
+    ;;
+    o) output_file="$OPTARG"
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    help
+    exit
+    ;;
+  esac
+done
+
+IFS=${delimiter:-,}
+
+if [ -z "${input_file}" ]; then
+    echo "ERROR! Input file not set."
+    help
+    exit
+fi
+
+if [ ! -f $input_file ]; then
+    echo "ERROR! Given file does not exist!"
+    help
+    exit
+fi
+
+output=${output_file:-"$input_file.html"}
 
 echo "Converting $input_file to HTML..."
 {
@@ -32,6 +74,6 @@ echo "Converting $input_file to HTML..."
     done
 
     echo "</table>"
-} < $input_file >> "${input_file}.html"
+} < $input_file >> $output
 
 exit
