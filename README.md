28996be4dd2bc212aab07c9fa630a56530278ef4
Marijus Siliunas <marijus.siliunas@gmail.com>
2016-09-17 16:03:52 +0300

    Fix argument parsing

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # CSV-to-HTML-Bash-Converter
+Script to convert CSV file to valid HTML table object.
+```
+Required arguments:
+	-i              Path to input file
+	--input-file
+Optional arguments:
+	-o              Output file path (default: .html)
+	--output-file
+
+	-d              Delimiter (default: ,)
+```
+
+Usage example:
+```
+./csv-html.sh -i test.csv -d ";" -o out.html
+```
--- csv-html.sh
+++ csv-html.sh
@@ -10,10 +10,13 @@
 function help {
     echo -e "Convert CSV file to valid HTML table.\n"
     echo "Required arguments:"
-    echo "	-i	Path to input file"
+    echo "	-i              Path to input file"
+    echo "	--input-file"
     echo "Optional arguments:"
-    echo "	-o	Output file path (default: $input_filename.html)"
-    echo "	-d	Delimiter (default: ,)"
+    echo "	-o              Output file path (default: $input_filename.html)"
+    echo -e "	--output-file\n"
+    echo "	-d              Delimiter (default: ,)"
+    echo "	--delimiter"
 }
 
 if [ $# -eq 0 ]; then
@@ -21,22 +24,29 @@ if [ $# -eq 0 ]; then
     exit
 fi
 
-while getopts ":o:i:d" opt; do
-    case $opt in
-    i) input_file="$OPTARG"
-    ;;
-    d) delimiter="$OPTARG"
-    ;;
-    o) output_file="$OPTARG"
-    ;;
-    \?) echo "Invalid option -$OPTARG" >&2
-    help
-    exit
-    ;;
-  esac
-done
+while [[ $# -gt 1 ]]; do
+key="$1"
 
-IFS=${delimiter:-,}
+case $key in
+    -i|--input-file)
+    input_file="$2"
+    shift # past argument
+    ;;
+    -d|--delimiter)
+    delimiter="$2"
+    shift # past argument
+    ;;
+    -o|--output-file)
+    output_file="$2"
+    shift # past argument
+    ;;
+    *)
+    help
+    exit        # unknown option
+    ;;
+esac
+shift # past argument or value
+done
 
 if [ -z "${input_file}" ]; then
     echo "ERROR! Input file not set."
@@ -51,11 +61,13 @@ if [ ! -f $input_file ]; then
 fi
 
 output=${output_file:-"$input_file.html"}
-
+echo "Delimiter $delimiter"
+IFS=${delimiter:-,}
+echo "Delimiter $IFS"
 echo "Converting $input_file to HTML..."
 {
     read -r -a headers
-    
+
     echo "<table><tr>"
 
     for header in "${headers[@]}"
@@ -65,7 +77,7 @@ echo "Converting $input_file to HTML..."
 
     echo "</tr>"
 
-    while read -r -a line; do
+    while IFS=${delimiter:-,} read -r -a line; do
         echo "<tr>"
         for i in "${!line[@]}"; do
             echo "<td>${line[i]}</td>"
